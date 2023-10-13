58b608843f6fa4c6b67d2211c1a79efc61da8369
Emmanuel-Mingala <emmanuel.mingala@tup.edu.ph>
2023-10-13 11:17:17 +0800

    Pagination

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -3,26 +3,41 @@ import MetaData from './Layout/Metadata'
 import axios from 'axios'
 import Product from './Product/Products'
 import Loader from './Layout/Loader'
+import Pagination from 'react-js-pagination'
 const Home = () => {
     const [loading, setLoading] = useState(true);
     const [products, setProducts] = useState([]);
     const [error, setError] = useState();
     const [productsCount, setProductsCount] = useState(0);
     const [currentPage, setCurrentPage] = useState(1);
+    const [resPerPage, setResPerPage] = useState(0);
+    
+    const getProducts = async (page=1) => {
+        let link='';
+        if (page){
+            link = `http://localhost:4001/api/v1/products?page=${page}`;
 
-    const getProducts = async () => {
-        let link = `http://localhost:4001/api/v1/products`
+        }
+        else{
+            link = `http://localhost:4001/api/v1/products`
+
+        }
+         
         console.log(link)
         let res = await axios.get(link)
         console.log(res);
         setProducts(res.data.products)
         setProductsCount(res.data.productsCount)
+        setResPerPage(res.data.resPerPage);
         setLoading(false)
     }
-
+    let count = productsCount;
+    function setCurrentPageNo(pageNumber) {
+        setCurrentPage(pageNumber)
+    }
     useEffect(() => {
-        getProducts()
-    }, []);
+        getProducts(currentPage)
+    }, [currentPage]);
     return (
 
         <Fragment>
@@ -38,6 +53,21 @@ const Home = () => {
                                 ))}
                             </div>
                         </section>
+                        {resPerPage <= count && (
+                            <div className="d-flex justify-content-center mt-5">
+                                <Pagination
+                                    activePage={currentPage}
+                                    itemsCountPerPage={resPerPage}
+                                    totalItemsCount={productsCount}
+                                    onChange={setCurrentPageNo}
+                                    nextPageText={'Next'}
+                                    prevPageText={'Prev'}
+                                    firstPageText={'First'}
+                                    lastPageText={'Last'}
+                                    itemClass="page-item"
+                                    linkClass="page-link"
+                                />
+                            </div>)}
                     </div>
                 </Fragment>
             )}
--- frontend/src/Components/Layout/Loader.js
+++ frontend/src/Components/Layout/Loader.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 const Loader = () => {
     return (
-        <div className="loader"> </div>
+        <div class="scene">
+            <div class="shadow"></div>
+            <div class="jumper">
+                <div class="spinner">
+                    <div class="scaler">
+                        <div class="loader">
+                            <div class="cuboid">
+                                <div class="cuboid__side"></div>
+                                <div class="cuboid__side"></div>
+                                <div class="cuboid__side"></div>
+                                <div class="cuboid__side"></div>
+                                <div class="cuboid__side"></div>
+                                <div class="cuboid__side"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
